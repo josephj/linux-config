e7e0d7952bedb396cfd4718bb0ef5ddda1f0db11
Ting-yu (Joseph) Chiang <josephj6802@gmail.com>
2011-11-21 16:29:08 +0800

    Update prompt.sh

--- prompt.sh
+++ prompt.sh
@@ -2,7 +2,7 @@
 
 echo "Please input your user name for Git configuration (ex. Joseph Chiang): "
 read user_name
-git config -g user.name $user_name
+git config --global user.name $user_name
 echo "Please input your email for Git configuration (ex. joseph_chiang@miiicasa.com): "
 read user_email
-git config -g user.email $user_email
+git config --global user.email $user_email
