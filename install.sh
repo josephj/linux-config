29bc29f433c6f44c11967603ec6b62c3b6cb1a77
joseph_chiang <joseph_chiang@devm1.corp.miiicasa.com>
2011-11-21 16:24:35 +0800

    Remove interactive code and use writable repository.

--- install.sh
+++ install.sh
@@ -12,7 +12,7 @@ die() {
 
 [ -e "~/.config" ] && die "~/.config already exists."
 
-git clone git://github.com/miiicasa/linux-config.git "$CONFIG_HOME"
+git clone git@github.com:miiicasa/linux-config.git "$CONFIG_HOME"
 
 cd "$CONFIG_HOME"
 git submodule update --init
@@ -24,13 +24,6 @@ ln -s .config/.zshrc .zshrc
 ln -s .config/.tmux.conf .tmux.conf
 ln -s .config/.gitconfig .gitconfig
 ln -s .config/git_diff_wrapper git_diff_wrapper
-
-echo -n "Please input your user name for Git configuration (ex. Joseph Chiang): "
-read user_name
-git config -g user.name $user_name
-echo -n "Please input your email for Git configuration (ex. joseph_chiang@miiicasa.com): "
-read user_email
-git config -g user.email $user_email
 
 cd "${CONFIG_HOME}/.vim"
 git submodule update --init
