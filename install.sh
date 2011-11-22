90d616f09628285eb6f7634bb12893e54f2026c6
Joseph Jiang <josephj@josephj.com>
2011-11-22 10:58:24 +0800

    Use writable repository.

--- install.sh
+++ install.sh
@@ -12,7 +12,7 @@ die() {
 
 [ -e "~/.config" ] && die "~/.config already exists."
 
-git clone git://github.com/josephj/linux-config.git "$CONFIG_HOME"
+git clone git@github.com:josephj/linux-config.git "$CONFIG_HOME"
 
 cd "$CONFIG_HOME"
 git submodule update --init
